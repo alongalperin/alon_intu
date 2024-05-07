57fae393aadb79f619c0743e34604a19c11af53b
Alon Galperin <alon@Alons-MacBook-Air.local>
2024-05-07 09:09:08 +0300

    add text for testing

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <div className="App">
+      <p>From main</p>
       <LoadButton
         handleClick={handleLoadDataClick}
         stateMachineStatus={currentStateMachine}
